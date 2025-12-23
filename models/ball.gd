5ab72b5b6ed5711c6eb1f44742f2c6a1be65dad6
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2025-12-23 21:21:50 +0000

    moved logic of push to player

--- models/ball.gd
+++ models/ball.gd
@@ -2,62 +2,16 @@ extends CharacterBody2D
 class_name Ball
 
 const FRICTION := 1800.0
-const PUSH_FORCE := 700.0
 const MIN_SPEED := 20.0
 const NEARBY := 30.0
-const POSSESSION_DISTANCE = 15.0
-var owner_position = null
 
 func stop():
 	velocity = Vector2.ZERO
+
+func push(direction: Vector2, force: float):
+	velocity = direction * force
 	
-func hold(holder: Player):
-	holder.stop()
-	velocity = Vector2.ZERO
-	owner_position = holder.global_position
-	pass
-
-func _on_detection_body_entered(body: Node2D) -> void:
-	if body.is_in_group("ball"):
-		return
-		
-	if body is Player:
-		#var mouse_pos = get_global_mouse_position();
-		if body.target and body.target.distance_to(global_position) < NEARBY:
-			print("holding")
-			hold(body)
-		elif body.velocity.length() > MIN_SPEED or (body.target and body.target.distance_to(global_position) > NEARBY):
-			print("push")
-			#var x_multi = -1 if body.target.x < global_position.x else 1
-			#global_position = body.global_position + Vector2(x_multi * 5, 0)
-			#var direction :Vector2 = (mouse_pos - global_position).normalized()
-			var direction = (body.target - global_position).normalized()
-			var distance = global_position.distance_to(body.target)
-			var force = PUSH_FORCE
-			if distance < 100:
-				force *= 0.5
-			print("pushdir: ", direction)
-			print("pushforce: ", force)
-			velocity = direction * force
-			owner_position = null
-		else:
-			owner_position = null
-
-func _on_detection_body_exited(body: Node2D) -> void:
-	if body.is_in_group("ball"):
-		return
-		
-	if body is CharacterBody2D and body.global_position.distance_to(global_position) > 25:
-		owner_position = null
-
-
 func _physics_process(delta: float) -> void:
-	var mouse_pos = get_global_mouse_position()
-	if owner_position:
-		var dir :Vector2= (mouse_pos - owner_position).normalized()
-		global_position = owner_position + dir * POSSESSION_DISTANCE
-	
 	if velocity.length() > 0.0:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-	
 	move_and_slide()
--- models/player.gd
+++ models/player.gd
@@ -5,38 +5,61 @@ class_name Player
 
 const MAX_SPEED := 200.0
 const TARGET_NEARBY := 10.0
+const ACCEL := 400.0
+const PUSH_FORCE := 700.0
+const POSSESSION_DISTANCE = 15.0
 
 var target = null
-var ball_nearby:Ball
+var ball:Ball
 
 func stop():
 	velocity = Vector2.ZERO
 	target = null
 
+func push(towards: Vector2):
+	if ball:
+		var direction = (towards - global_position).normalized()
+		var distance = global_position.distance_to(towards)
+		var force = PUSH_FORCE
+		if distance < 100:
+				force *= 0.5
+		ball.push(direction, force)
+
 func select_target(new_target: Vector2):
+	if ball:
+		push(new_target)
 	target = new_target
 
-const ACCEL := 400.0
 
 func _physics_process(_delta: float) -> void:
-	if target != null and global_position.distance_to(target) > TARGET_NEARBY:
+	if target and target.distance_to(global_position) <= TARGET_NEARBY:
+		velocity = Vector2.ZERO
+		target = null
+		
+	if ball and target == null:
+		var mouse_pos = get_global_mouse_position()
+		var dir :Vector2= (mouse_pos - global_position).normalized()
+		ball.global_position = global_position + dir * POSSESSION_DISTANCE
+	if target and global_position.distance_to(target) > TARGET_NEARBY:
 		var direction = (target - global_position).normalized()
 		velocity = velocity.move_toward(direction * MAX_SPEED, ACCEL * _delta)
 	elif target != null:
 		velocity = Vector2.ZERO
 		target = null
-		if ball_nearby:
-			ball_nearby.hold(self)
-		
-
 	move_and_slide()
 
 
 func _on_ball_possession_body_entered(body: Node2D) -> void:
 	if body is Ball:
-		ball_nearby = body
+		ball = body
+		if target == null or target.distance_to(global_position) < TARGET_NEARBY:
+			velocity = Vector2.ZERO
+			target = null
+			ball.stop()
+		else:
+			push(target)
 
 
 func _on_ball_possession_body_exited(body: Node2D) -> void:
 	if body is Ball:
-		ball_nearby = null
+		ball = null
