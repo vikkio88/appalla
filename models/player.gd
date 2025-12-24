e46eba03e8c902176f141585b30b345280a4a4e1
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2025-12-24 07:17:12 +0000

    fixed basic movements

--- models/player.gd
+++ models/player.gd
@@ -4,7 +4,7 @@ class_name Player
 @onready var sprite := $sprite
 
 const MAX_SPEED := 200.0
-const TARGET_NEARBY := 10.0
+const TARGET_NEARBY := 20.0
 const ACCEL := 400.0
 const PUSH_FORCE := 700.0
 const POSSESSION_DISTANCE = 15.0
@@ -32,29 +32,27 @@ func select_target(new_target: Vector2):
 
 
 func _physics_process(_delta: float) -> void:
-	if target and target.distance_to(global_position) <= TARGET_NEARBY:
-		velocity = Vector2.ZERO
-		target = null
+	var is_target_nearby = target and target.distance_to(global_position) <= TARGET_NEARBY;
+	if is_target_nearby:
+		stop()
 		
 	if ball and target == null:
 		var mouse_pos = get_global_mouse_position()
 		var dir :Vector2= (mouse_pos - global_position).normalized()
 		ball.global_position = global_position + dir * POSSESSION_DISTANCE
-	if target and global_position.distance_to(target) > TARGET_NEARBY:
+	if target and not is_target_nearby:
 		var direction = (target - global_position).normalized()
 		velocity = velocity.move_toward(direction * MAX_SPEED, ACCEL * _delta)
 	elif target != null:
-		velocity = Vector2.ZERO
-		target = null
+		stop()
 	move_and_slide()
 
 
 func _on_ball_possession_body_entered(body: Node2D) -> void:
 	if body is Ball:
 		ball = body
-		if target == null or target.distance_to(global_position) < TARGET_NEARBY:
-			velocity = Vector2.ZERO
-			target = null
+		if target == null or target.distance_to(ball.global_position) < 15 or target.distance_to(global_position) < TARGET_NEARBY:
+			stop()
 			ball.stop()
 		else:
 			push(target)
